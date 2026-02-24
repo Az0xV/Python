b84c62718f0e5eafd46e06d5e3c79c56832ec261
Az0xV <135340856+Az0xV@users.noreply.github.com>
2026-02-24 11:06:03 +0100

    Update TableFormat.py

--- Common/TableFormat.py
+++ Common/TableFormat.py
@@ -1,4 +1,34 @@
-﻿# Comment this script
+﻿"""
+Example use:
+table = TableService() # Create new TableService object
+
+col1 = TableColumn("Name", 10, ["Alice", "Bob", "Charlie"])
+col2 = TableColumn("Age", 5, [23, 34, 29])
+col3 = TableColumn("Score", 8, [95.456, 88.1, 76.9876])
+
+# These 3 lines, creates new TableColumn object which TableService uses with example values.
+# TableColumn needs 3 variables in this order:
+# Column name
+# Column length (if less than column name, use column name length)
+# Column data (array)
+
+table.addColumn(col1)
+table.addColumn(col2)
+table.addColumn(col3)
+# This adds columns to the table
+
+table.displayTable()
+# Displays created table, should look like this:
+#------------------------------------
+# | Name       |  | Age  |  | Score   |
+# | Alice      |  | 23   |  | 95.46   |
+# | Bob        |  | 34   |  | 88.1    |
+# | Charlie    |  | 29   |  | 76.99   |
+#------------------------------------
+
+# In the table, the row is colored so as not to get confused when looking at a large number of values.
+"""
+
 import colorama
 
 colorama.init()
@@ -24,8 +54,7 @@ class TableService:
         for width in widthForColumn:
             print(" | " + width[0] + " | ",end="")
         print()
-        
-        # Find the maximum number of values across all columns
+        
         max_rows = max(len(column.values) if column.values else 0 for column in self.columns)
 
         for row in range(max_rows):
